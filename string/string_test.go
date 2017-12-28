a247ab038acf973a6aa6e4e65f8d7fc04c697674
Naruto <ahmadsururi@live.com>
2017-12-28 12:29:55 +0700

    generate string

---
+++ string/string_test.go
@@ -0,0 +1,15 @@
+package string
+
+import (
+	"testing"
+)
+
+func TestGenerateSting(t *testing.T) {
+	generateNow := RandomStr(28, "nozero")
+
+	var expectedResult = "dkdhsk"
+
+	if generateNow != expectedResult {
+		t.Fatalf("expected %s but Got %s", expectedResult, generateNow)
+	}
+}
